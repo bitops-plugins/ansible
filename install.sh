626dd73d771f0b7458b76513345bb447eeb43030
Shyam <shyam.rayaprolu@gmail.com>
2022-06-15 22:20:47 -0400

    ansible install script

--- install.sh
+++ install.sh
@@ -7,19 +7,25 @@ echo "It should be used to install any dependencies required to actually run you
 echo "BitOps uses alpine linux as its base, so you'll want to use apk commands (Alpine Package Keeper)"
 echo ""
 
-#apk info
-
-mkdir -p /opt/download
-cd /opt/download
-
-function install_terraform() {
-    while IFS='' read -r version; do
-        TERRAFORM_DOWNLOAD_URL="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"
-        echo ${TERRAFORM_DOWNLOAD_URL}
-        curl -LO ${TERRAFORM_DOWNLOAD_URL} && unzip terraform_${version}_linux_amd64.zip -d ./
-        mv terraform /usr/local/bin/terraform-${version}
-        chmod +x /usr/local/bin/terraform-${version}
-    done <<< "$TERRAFORM_VERSIONS"
-}
-
-install_terraform
+if ! command -v ansible >/dev/null; then
+        echo "Installing Ansible dependencies and Git."
+        if command -v yum >/dev/null; then
+                sudo yum install -y git python python-devel
+        elif command -v apt-get >/dev/null; then
+                sudo apt-get update -qq
+                #sudo apt-get install -y -qq git python-yaml python-paramiko python-jinja2
+                sudo apt-get install -y -qq git python python-dev
+        else
+                echo "neither yum nor apt-get found!"
+                exit 1
+        fi
+        echo "Installing ansible via pip3 "
+        # wget http://peak.telecommunity.com/dist/ez_setup.py
+        # sudo python ez_setup.py && rm -f ez_setup.py
+        # sudo easy_install pip
+        # # Make sure setuptools are installed crrectly.
+        # sudo pip install setuptools --no-use-wheel --upgrade
+        # echo "Installing required python modules."
+        # sudo pip install paramiko pyyaml jinja2 markupsafe
+        sudo pip3 install ansible
+fi
