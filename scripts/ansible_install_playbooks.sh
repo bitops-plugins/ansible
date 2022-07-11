de2d657a848ee3f69b0bbdced767e3e351708014
Shyam <shyam.rayaprolu@gmail.com>
2022-07-11 11:22:37 -0400

    echo statement

--- scripts/ansible_install_playbooks.sh
+++ scripts/ansible_install_playbooks.sh
@@ -4,7 +4,7 @@ set -e
 ANSIBLE_ARGS=$1
 PLUGIN_DIR="$BITOPS_OPSREPO_ENVIRONMENT_DIR"
 
-echo "Running ansible_install_playbook.sh for $PLUGIN_DIR"
+echo "Running ansible_install_playbook.sh for $PLUGIN_DIR with args $ANSIBLE_ARGS"
 
 for playbook in $(ls $PLUGIN_DIR/*[^bitops.config].yaml || ls $PLUGIN_DIR/*[^bitops.config].yml); do
     echo "Executing playbook: $playbook"
