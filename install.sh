c63ac3cf20bb9727c55b242188d0aaf142a7f22b
Shyam <shyam.rayaprolu@gmail.com>
2022-07-11 15:34:24 -0400

    testing again

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ echo ""
 
 echo "Installing ansible"
 
+apk add ansible
+
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
-apk add ansible
+
 pipx install ansible-base
 
 ansible --version | grep "python version"
