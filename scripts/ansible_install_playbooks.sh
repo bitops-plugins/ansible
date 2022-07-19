9fcedc933814efbd072f02f0d6d25b5b3fc3eeb0
Phil Henning <philh@bitovi.com>
2022-07-19 14:36:44 -0400

    Update ansible_install_playbooks.sh

--- scripts/ansible_install_playbooks.sh
+++ scripts/ansible_install_playbooks.sh
@@ -6,7 +6,12 @@ PLUGIN_DIR="$BITOPS_OPSREPO_ENVIRONMENT_DIR"
 
 echo "Running ansible_install_playbook.sh for $PLUGIN_DIR with args $ANSIBLE_ARGS"
 
-for playbook in $(ls $PLUGIN_DIR/*[^bitops.config].yaml || ls $PLUGIN_DIR/*[^bitops.config].yml); do
-    echo "Executing playbook: $playbook"
-    ansible-playbook $playbook $ANSIBLE_ARGS
-done
+PLAYBOOK="$PLUGIN_DIR/$BITOPS_MAIN_SCRIPT"
+
+ansible-playbook $PLAYBOOK $ANSIBLE_ARGS
+
+
+# for playbook in $(ls $PLUGIN_DIR/*[^bitops.config].yaml || ls $PLUGIN_DIR/*[^bitops.config].yml); do
+#     echo "Executing playbook: $playbook"
+#     ansible-playbook $playbook $ANSIBLE_ARGS
+# done
