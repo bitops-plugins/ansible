383ff6f04d1f1412c8d60f20ad89918f51640076
Phil Henning <philh@bitovi.com>
2022-11-03 15:46:46 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,10 @@ export BITOPS_CONFIG_SCHEMA="$BITOPS_PLUGIN_DIR/bitops.schema.yaml'"
 export SCRIPTS_DIR="$BITOPS_SCRIPTS_DIR"
 export PLUGIN_DIR="$BITOPS_OPSREPO_ENVIRONMENT_DIR"
 
+if [ "$SKIP_DEPLOY_ANSIBLE" == "true" ]; then
+  echo "SKIP_DEPLOY_ANSIBLE is set.  Skipping."
+  exit 0
+fi
 
 echo "Running Ansible Playbooks..."
 
